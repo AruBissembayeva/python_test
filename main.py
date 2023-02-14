8c050bb92ed1269f5f89c1abc37aaf2f852689e1
Aru <aru03ka@gmail.com>
2023-02-14 11:54:09 +0600

    labka_2

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 print ("Aru Bissembayeva - student of Kaznitu.")
 
 x = 5
-if x < 5:
+if x>5 :
     print ("menshe")
-elif x==5 :
+elif x ==5:
     print ("rovno")
 else :
-    print("menshe")
+    print ("menshe")
 # 10-5=5
-
